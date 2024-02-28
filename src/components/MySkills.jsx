fc1ec7cb5beb3370d91078500d0b6633faf7107c
hablu <131282586+hablu@users.noreply.github.com>
2024-02-28 20:45:20 +0600

    skill added

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -5,8 +5,161 @@ import Marquee from "react-fast-marquee";
 const MySkills = () => {
   return (
     <div>
-      <Marquee>
-        <Image src='/public/' />
+      <Marquee className=" ">
+        <Image
+          className=" mx-5"
+          src="/js.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/next.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/react.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/html.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/css.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/node-js.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/express.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/tailwind.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/mui.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/mongodb.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/figma.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/Firebase.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/framer.png"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
+        <Image
+          className=" mx-5"
+          src="/prisma.webp"
+          alt="js"
+          width={80}
+          height={80}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+        />
       </Marquee>
     </div>
   );
